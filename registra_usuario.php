8a61673a1b9b681df82b2c364263942b0fa269ba
ianporto1 <iangomesporto@gmail.com>
2021-11-02 15:22:11 -0300

    Condições

--- registra_usuario.php
+++ registra_usuario.php
@@ -4,10 +4,22 @@
 
     $usuario = $_POST['usuario'];
     $email = $_POST['email'];
-    $senha = $_POST['senha'];
+    $senha = md5($_POST['senha']);
 
     $objDb = new db();
     $link = $objDb->conecta_mysql();
+
+    $sql = " select * from usuarios where usuario = '$usuario' ";
+    if (mysqli_query($link,$sql)){
+
+      $dados_usuario = mysqli_fetch_array ( $resultado_id)  mysqli_fetch_array($resultado_id);
+
+    }else {
+        echo ' Erro ao tentar localizar o registro de usuário';
+
+    }
+
+    die();
 
     $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
 
